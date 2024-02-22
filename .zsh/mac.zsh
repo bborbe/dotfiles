a5eb6c4669e351002bc1b2f87b551caeb14343cd
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-02-22 15:27:13 +0100

    add k8sfire alias

--- .zsh/mac.zsh
+++ .zsh/mac.zsh
@@ -8,6 +8,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	alias k8sdev="kubectl config use-context gke_gce-smedia-k8s_europe-west3-c_dev"
 	alias k8sstaging="kubectl config use-context gke_gce-smedia-k8s_europe-west3-c_staging"
 	alias k8sprod="kubectl config use-context gke_gce-smedia-k8s_europe-west3-c_prod"
+	alias k8sfire="kubectl config use-context fire"
 
 	# Golang
 	export GOPATH=$HOME/Documents/workspaces/go
