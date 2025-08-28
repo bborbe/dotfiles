91e0ee1d0b832a6edf44611aad6785a4ebd39dbe
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-08-28 14:49:20 +0200

    load keychain ssh keys

--- .zsh/mac.zsh
+++ .zsh/mac.zsh
@@ -3,8 +3,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
 	defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-  ssh-add --apple-use-keychain ~/.ssh/id_ed25519_personal
-  ssh-add --apple-use-keychain ~/.ssh/id_ed25519_work
+  ssh-add --apple-load-keychain
 
 	# Golang
 	export GOPATH=$HOME/Documents/workspaces/go
