01f241a3c2d11e5399875456832063f6d43c132e
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-09-09 21:36:18 +0200

    rename fire -> quant

--- .zsh/k8s.zsh
+++ .zsh/k8s.zsh
@@ -3,8 +3,9 @@
 alias k8sdev="kubectl config use-context gke_gce-smedia-k8s_europe-west3-c_dev"
 alias k8sstaging="kubectl config use-context gke_gce-smedia-k8s_europe-west3-c_staging"
 alias k8sprod="kubectl config use-context gke_gce-smedia-k8s_europe-west3-c_prod"
-alias k8sfire="kubectl config use-context fire"
+alias k8squant="kubectl config use-context quant"
 alias k8shell="kubectl config use-context hell"
+alias k8sfire="echo 'use k8squant'"
 
 # Kubectl with content
 alias kubectldev="kubectl --context gke_gce-smedia-k8s_europe-west3-c_dev"
