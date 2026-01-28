022099c253ca536bb258c9a9bead558c06df7a02
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-01-28 13:58:28 +0100

    move nvm and .local to default

--- .zsh/default.zsh
+++ .zsh/default.zsh
@@ -1,1 +1,9 @@
 export DOTFILES=~/dotfiles
+
+# init nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# usx to PATH
+. "$HOME/.local/bin/env"
