e531d03a8e8d82170824ed2546e9ebaa1dc9d468
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-07-02 09:17:03 +0200

    update wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,13 +5,25 @@ local config = wezterm.config_builder()
 local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
 local is_macos = wezterm.target_triple:lower():find("darwin") ~= nil
 
-config.color_scheme = "rose-pine-moon"
+config.color_scheme = "Solarized Dark (Gogh)"
+--config.color_scheme = "OneDark (base16)"
+--config.color_scheme = "nord"
+--config.color_scheme = "Dracula (Official)"
+--config.color_scheme = "Gruvbox Material (Gogh)"
+--config.color_scheme = "Catppuccin Macchiato"
+--config.color_scheme = "Catppuccin Mocha"
+--config.color_scheme = "Tokyo Night"
+--config.color_scheme = "Tokyo Night Storm"
+--config.color_scheme = "rose-pine-moon"
+
 config.max_fps = 120
-config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
+-- config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
+config.font = wezterm.font("Monaco")
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_frame = {
   font = wezterm.font("Hack Nerd Font", { weight = "Bold" }),
 }
+
 config.inactive_pane_hsb = {
   saturation = 0.0,
   brightness = 0.5,
@@ -19,15 +31,15 @@ config.inactive_pane_hsb = {
 
 if is_windows then
   config.win32_system_backdrop = "Acrylic"
-  config.window_background_opacity = 0.7
-  config.window_frame.font_size = 10.0
+  config.window_background_opacity = 1.0
+  config.window_frame.font_size = 12.0
 end
 
 if is_macos then
-  config.window_background_opacity = 0.8
+  config.window_background_opacity = 1.0
   config.macos_window_background_blur = 50
-  config.font_size = 15.0
-  config.window_frame.font_size = 13.0
+  config.font_size = 12.0
+  config.window_frame.font_size = 12.0
 end
 
 return config
